5d7507ab76ddcf38a8090587c77e0e384558bdae
khari thomas <tkhari77@gmail.com>
2023-08-02 17:45:01 -0500

    add solution for valid palindrome

---
+++ arrays_hashing/valid_palindrome.py
@@ -0,0 +1,32 @@
+from typing import List
+
+# TC : O(n)
+# SC : O(1)
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        start, end = 0, len(s) - 1
+
+        while start < end:
+            if not s[start].lower().isalnum():
+                start += 1
+                continue
+
+            if not s[end].lower().isalnum():
+                end -= 1
+                continue
+
+            if s[start].lower() != s[end].lower():
+                return False
+
+            start += 1
+            end -= 1
+
+        return True
+
+
+s = Solution()
+print(s.isPalindrome("A man, a plan, a canal: Panama"))
+print(s.isPalindrome("race a car"))
+print(s.isPalindrome(" "))
