36bc6123327a0beda2ac9caf5a952ae22f33b75f
khari thomas <tkhari77@gmail.com>
2023-08-02 16:50:39 -0500

    add solution for two sum ii sorted input array

---
+++ arrays_hashing/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,25 @@
+from typing import List
+
+# TC : O(N)
+# SC : O(1)
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        start, end = 0, len(numbers) - 1
+
+        while start < end:
+            sum = numbers[start] + numbers[end]
+
+            if sum == target:
+                return [start + 1, end + 1]
+            elif sum > target:
+                end -= 1
+            else:
+                start += 1
+
+
+s = Solution()
+print(s.twoSum([2, 7, 11, 15], 9))
+print(s.twoSum([2, 3, 4], 6))
+print(s.twoSum([-1, 0], -1))
