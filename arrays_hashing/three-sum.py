962ba04bea4ecea400e693ca00b4decefe3bf221
khari thomas <tkhari77@gmail.com>
2023-08-03 22:03:36 -0500

    add solution for three sum

---
+++ arrays_hashing/three-sum.py
@@ -0,0 +1,35 @@
+from typing import List
+
+# TC : O(N^2)
+# SC : O(N)
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            start, end = i + 1, len(nums) - 1
+            while start < end:
+                sum = nums[i] + nums[start] + nums[end]
+                if sum > 0:
+                    end -= 1
+                elif sum < 0:
+                    start += 1
+                else:
+                    res.append([nums[i], nums[start], nums[end]])
+                    start += 1
+                    while start < end and nums[start] == nums[start - 1]:
+                        start += 1
+
+        return res
+
+
+s = Solution()
+print(s.threeSum([-1, 0, 1, 2, -1, -4]))
+print(s.threeSum([0, 1, 1]))
+print(s.threeSum([0, 0, 0]))
