7fcfabe05b66f07401e97950ebeefe6ff46fdd41
khari thomas <tkhari77@gmail.com>
2023-08-06 13:06:54 -0500

    add solution for reverse words in a string

---
+++ two_pointers/reverse-words-in-a-string.py
@@ -0,0 +1,31 @@
+# TC : O(N)
+# SC : O(N)
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        res = ""
+        left = 0
+        str_len = len(s)
+
+        while left < str_len:
+            while left < str_len and s[left] == " ":
+                left += 1
+
+            if left >= str_len:
+                break
+
+            right = left + 1
+            while right < str_len and s[right] != " ":
+                right += 1
+
+            word = s[left:right]
+            res = word if not res else f"{word} {res}"
+            left = right + 1
+
+        return res
+
+
+s = Solution()
+print(s.reverseWords("the sky is blue"))
+print(s.reverseWords("  hello world  "))
+print(s.reverseWords("a good   example"))
