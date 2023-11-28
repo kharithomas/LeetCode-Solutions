872ce564ce14fe810de1b8bb8e64f8979cc25bae
khari thomas <tkhari77@gmail.com>
2023-11-27 21:01:23 -0600

    update solution for contains duplicate

---
+++ arrays_hashing/contains_duplicates.py
@@ -0,0 +1,23 @@
+# TC : O(n)
+# SC : O(n)
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for n in nums:
+            if n in seen:
+                return True
+
+            seen.add(n)
+
+        return False
+
+
+s = Solution()
+print(s.containsDuplicate([1, 2, 3, 1]))
+print(s.containsDuplicate([1, 2, 3, 4]))
+print(s.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
