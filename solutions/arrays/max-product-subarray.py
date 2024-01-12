f5cf95afd067b2097d023266944a01b60badd634
khari thomas <tkhari77@gmail.com>
2024-01-11 20:24:04 -0600

    fix solution for max product subarray

--- solutions/arrays/max-product-subarray.py
+++ solutions/arrays/max-product-subarray.py
@@ -7,14 +7,17 @@ from typing import List
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         max_product = nums[0]
-        curr_product = 1
+        curr_max, curr_min = 1, 1
 
         for n in nums:
-            curr_product = max(curr_product * n, n)
-            max_product = max(curr_product, max_product)
+            temp = curr_max * n
+            curr_max = max(curr_max * n, curr_min * n, n)
+            curr_min = min(temp, curr_min * n, n)
+            max_product = max(curr_max, max_product)
 
         return max_product
 
 
 s = Solution()
-print(s.maxProduct([-2, 3, -4]))
+print(s.maxProduct([2, 3, -2, 4]))
+print(s.maxProduct([-2, 0, -1]))
