2522743e6d952ae634a0df284ef720cbceb4c919
khari thomas <tkhari77@gmail.com>
2024-01-14 20:21:49 -0600

    add solution for container with most water

---
+++ solutions/arrays/container-with-most-water.py
@@ -0,0 +1,26 @@
+# TC: O(n)
+# SC: O(1)
+
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = float('-inf')
+        left, right = 0, len(height) - 1
+
+        while left < right:
+            curr_area = (right - left) * min(height[right], height[left])
+            max_area = max(max_area, curr_area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
+
+s = Solution()
+print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(s.maxArea([1, 1]))
