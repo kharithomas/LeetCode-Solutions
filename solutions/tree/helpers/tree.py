7e2892e350361aa5c5855655094de05696f24a70
khari thomas <tkhari77@gmail.com>
2024-06-04 12:25:19 -0500

    add solution for flip binary tree

---
+++ solutions/tree/helpers/tree.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from collections import deque
+
+
+class TreeNode:
+    """A single node of a Trie"""
+
+    def __init__(self, value: int) -> None:
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def print_tree(root: Optional[TreeNode]) -> None:
+    if not root:
+        return
+
+    queue = deque([root])
+    while queue:
+        node = queue.popleft()
+        print(node.value, end=", ")
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
---
+++ solutions/tree/invert-binary-tree.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from helpers.tree import TreeNode, print_tree
+
+# TC : O(N)
+# SC : O(N)
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is not None:
+            # flip subtree
+            root.right, root.left = root.left, root.right
+
+            # recurse on left subtree
+            root.left = self.invertTree(root.left)
+
+            # recurse on right subtree
+            root.right = self.invertTree(root.right)
+
+        return root
+
+
+s = Solution()
+
+b = TreeNode(2)
+b.left = TreeNode(1)
+b.right = TreeNode(3)
+
+c = TreeNode(7)
+c.left = TreeNode(6)
+c.right = TreeNode(9)
+
+a = TreeNode(4)
+a.left = b
+a.right = c
+
+print("Before Flip: ", end="")
+print_tree(a)
+print("")
+
+s.invertTree(a)
+
+print("After Flip: ", end=" ")
+print_tree(a)
