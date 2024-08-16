7edb60f062ca23dc1d4412b31d62b2ae26396000
khari thomas <tkhari77@gmail.com>
2024-08-16 17:03:39 -0500

    fix solution

--- solutions/linked-list/lru-cache.py
+++ solutions/linked-list/lru-cache.py
@@ -7,62 +7,42 @@ class ListNode:
 
 class LRUCache:
     def __init__(self, capacity: int):
-        """
-        Initialize the LRU cache with a given capacity.
-        """
         self.capacity = capacity
         self.cache = {}
+        self.head = ListNode()  # LRU
+        self.tail = ListNode()  # MRU
 
-        self.lru = ListNode()  # Least Recently Used (dummy head)
-        self.mru = ListNode()  # Most Recently Used (dummy tail)
-        self.lru.next = self.mru
-        self.mru.prev = self.lru
+        self.head.prev = self.tail
+        self.tail.next = self.head
 
-    def remove(self, node: ListNode):
-        """
-        Remove a node from the doubly linked list.
-        """
-        prev = node.prev
-        next = node.next
-        prev.next = next
-        next.prev = prev
+    def _remove(self, node: ListNode):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
 
-    def insert(self, node: ListNode):
-        """
-        Insert a node right before the most recently used (MRU) node.
-        """
-        last_node = self.mru.prev
-        last_node.next = node
-        node.prev = last_node
-        self.mru.prev = node
+    def _insert(self, node: ListNode):
+        prev, nxt = self.head.prev, self.head
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
-        """
-        Return the value of the node with the given key if present, 
-        otherwise return -1.
-        """
-        if key not in self.cache:
-            return -1
+        if key in self.cache:
+            self._remove(self.cache[key])
+            self._insert(self.cache[key])
+            return self.cache[key].val
 
-        node = self.cache[key]
-        self.remove(node)
-        self.insert(node)
-        return node.val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        """
-        Add a new key-value pair to the cache or update the value of an existing key.
-        """
         if key in self.cache:
-            self.remove(self.cache[key])
+            self._remove(self.cache[key])
 
         self.cache[key] = ListNode(key, value)
-        self.insert(self.cache[key])
+        self._insert(self.cache[key])
 
         if len(self.cache) > self.capacity:
-            node = self.lru.next
-            self.remove(node)
-            del self.cache[node.key]
+            lru = self.tail.next
+            self._remove(lru)
+            del self.cache[lru.key]
 
 
 lru_cache = LRUCache(2)
