74661eb0234b76086b8e14a41df01949daa13fbf
khari thomas <tkhari77@gmail.com>
2025-08-11 17:28:27 -0500

    improve readability of diagonal traverse solution

--- solutions/matrix/diagonal-traverse.py
+++ solutions/matrix/diagonal-traverse.py
@@ -1,44 +1,42 @@
 # TC: O(m*n)
-# SC: O(1)
+# SC: O(1), extra-space
 
 from typing import List
 
+
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        if not mat or not mat[0]:
-            return []
-
         rows, cols = len(mat), len(mat[0])
-        y, x = 0, 0
-        up = True
         res = []
+        r, c = 0, 0
+        direction = 1  # up
 
-        while len(res) < rows * cols:
-            res.append(mat[y][x])
+        while r < rows and c < cols:
+            # add current val to result
+            res.append(mat[r][c])
 
-            if up:
-                if x == cols - 1:  # If at last column, move down
-                    y += 1
-                    up = False
-                elif y == 0:  # If at first row, move right
-                    x += 1
-                    up = False
-                else:  # Otherwise, move diagonally up-right
-                    y -= 1
-                    x += 1
+            # if we're in the (first col and not last row and going down) or (last col and going up), go down row (incr. row)
+            if (c == 0 and direction < 0 and r < rows - 1) or (c == cols - 1 and direction > 0):
+                r += 1
+                direction *= -1
+            # elif we're in the (first row and going up) or (last row and going down), go right (incr. col)
+            elif (r == 0 and direction > 0) or (r == rows - 1 and direction < 0):
+                c += 1
+                direction *= -1
+            # else, go diagonally based on direction
             else:
-                if y == rows - 1:  # If at last row, move right
-                    x += 1
-                    up = True
-                elif x == 0:  # If at first column, move down
-                    y += 1
-                    up = True
-                else:  # Otherwise, move diagonally down-left
-                    y += 1
-                    x -= 1
-        
+                # if dir is up, move add col, sub row
+                if direction > 0:
+                    c += 1
+                    r -= 1
+                # else dir is down, sub col, add row
+                else:
+                    c -= 1
+                    r += 1
+
         return res
-    
+
+
 s = Solution()
-print(s.findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))
-print(s.findDiagonalOrder([[1,2],[3,4]]))
+print(s.findDiagonalOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+print(s.findDiagonalOrder([[1, 2], [3, 4]]))
